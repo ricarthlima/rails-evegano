59347e6567d14cad4504dd25d81ac52d94f5b324
Warley <wss@cin.ufpe.br>
2018-11-05 19:37:14 +0000

    adicionado mais campos no cadastro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_023509) do
+ActiveRecord::Schema.define(version: 2018_11_05_185302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_023509) do
     t.string "pais"
     t.string "telefone"
     t.integer "tipo_usuario"
+    t.boolean "dev"
     t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
