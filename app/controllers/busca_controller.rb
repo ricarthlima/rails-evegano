cadf24fb116cd89cf7d542068a61056bc91a4d34
Ricarth Lima <rrsl@cin.ufpe.br>
2018-11-11 23:23:54 +0000

    implementada busca simples

--- app/controllers/busca_controller.rb
+++ app/controllers/busca_controller.rb
@@ -1,8 +1,26 @@
 class BuscaController < ApplicationController
     def buscar
-        @nome_busca = params[:nome]
-        puts("camelo")
+        @nome_busca = params[:texto]
+        
+        @candidatos_produtos = Produto.where "nome like ?", "%#{@nome_busca}%"
+        @candidatos_marcas = Marca.where "nome like ?", "%#{@nome_busca}%"
+        @candidatos_componentes = Componente.where "nome like ?", "%#{@nome_busca}%"
+        
+        puts("# TDD haha---------------------")
         puts(@nome_busca)
-        redirect_to(inicio_path)
+        puts(@candidatos_produtos.size)
+        puts(@candidatos_marcas.size)
+        puts(@candidatos_componentes.size)
+        puts("# TDD hah---------------------")
+        
+        if @candidatos_produtos.size == 1
+            redirect_to(produtos_url+"/"+@candidatos_produtos[0].id.to_s)
+        elsif @candidatos_marcas.size == 1
+            redirect_to(marcas_url+"/"+@candidatos_marcas[0].id.to_s)
+        elsif @candidatos_componentes.size == 1
+            redirect_to(componentes_url+"/"+@candidatos_componentes[0].id.to_s)
+        else
+            redirect_to(inicio_path)
+        end
     end
 end
