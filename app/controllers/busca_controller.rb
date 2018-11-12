3f9efb3ea7d07a00978d14586dfc30b43019b0df
Ricarth Lima <rrsl@cin.ufpe.br>
2018-11-12 00:39:56 +0000

    issue#87 - enviar busca

--- app/controllers/busca_controller.rb
+++ app/controllers/busca_controller.rb
@@ -12,12 +12,15 @@ class BuscaController < ApplicationController
         @candidatos_marcas = Marca.where "lower(nome) like lower(?)", "%#{@nome_busca}%"
         @candidatos_componentes = Componente.where "lower(nome) like lower(?)", "%#{@nome_busca}%"
         
-        if @candidatos_produtos.size == 1
+        if @candidatos_produtos.size == 1 and (@candidatos_marcas.size == 0) and (@candidatos_componentes.size == 0)
             redirect_to(produtos_url+"/"+@candidatos_produtos[0].id.to_s)
-        elsif @candidatos_marcas.size == 1
+            
+        elsif @candidatos_marcas.size == 1 and (@candidatos_produtos.size == 0) and (@candidatos_componentes.size == 0)
             redirect_to(marcas_url+"/"+@candidatos_marcas[0].id.to_s)
-        elsif @candidatos_componentes.size == 1
+            
+        elsif @candidatos_componentes.size == 1 and (@candidatos_marcas.size == 0) and (@candidatos_produtos.size == 0)
             redirect_to(componentes_url+"/"+@candidatos_componentes[0].id.to_s)
+            
         elsif @candidatos_componentes.size == 0 and @candidatos_produtos.size == 0 and @candidatos_marcas.size == 0
             redirect_to(inicio_path)
         else
