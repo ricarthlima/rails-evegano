446a3374f0277db987af6f2ef6a935fd6c1cd1d9
Ricarth Lima <rrsl@cin.ufpe.br>
2018-11-12 00:28:32 +0000

    criada a tela de casos multiplos

--- app/controllers/busca_controller.rb
+++ app/controllers/busca_controller.rb
@@ -1,17 +1,16 @@
 class BuscaController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+
     def buscar
         @nome_busca = params[:texto]
         
-        @candidatos_produtos = Produto.where "nome like ?", "%#{@nome_busca}%"
-        @candidatos_marcas = Marca.where "nome like ?", "%#{@nome_busca}%"
-        @candidatos_componentes = Componente.where "nome like ?", "%#{@nome_busca}%"
-        
-        puts("# TDD haha---------------------")
-        puts(@nome_busca)
-        puts(@candidatos_produtos.size)
-        puts(@candidatos_marcas.size)
-        puts(@candidatos_componentes.size)
-        puts("# TDD hah---------------------")
+        @candidatos_produtos = Produto.where "lower(nome) like lower(?)", "%#{@nome_busca}%"
+        @candidatos_marcas = Marca.where "lower(nome) like lower(?)", "%#{@nome_busca}%"
+        @candidatos_componentes = Componente.where "lower(nome) like lower(?)", "%#{@nome_busca}%"
         
         if @candidatos_produtos.size == 1
             redirect_to(produtos_url+"/"+@candidatos_produtos[0].id.to_s)
@@ -19,8 +18,12 @@ class BuscaController < ApplicationController
             redirect_to(marcas_url+"/"+@candidatos_marcas[0].id.to_s)
         elsif @candidatos_componentes.size == 1
             redirect_to(componentes_url+"/"+@candidatos_componentes[0].id.to_s)
-        else
+        elsif @candidatos_componentes.size == 0 and @candidatos_produtos.size == 0 and @candidatos_marcas.size == 0
             redirect_to(inicio_path)
+        else
+            render :index
         end
     end
+    
+    private 
 end
