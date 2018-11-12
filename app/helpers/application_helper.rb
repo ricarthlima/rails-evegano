2a1b09ed3961141656998e0fe8d3f168d7998bcd
Ricarth Lima <rrsl@cin.ufpe.br>
2018-11-12 01:10:19 +0000

    issue#51 - tela de produto

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+    def class_pmc_vegano(i)
+        case i
+        when 0
+            return "Vegano"
+        when 1
+            return "Potencialmente Não-Vegano"
+        when 2
+            return "Não-vegano"
+        end
+    end
+    
+    def class_usuario_vegano(i)
+        case i
+        when 0
+            return "Vegano"
+        when 1
+            return "Vegetariano"
+        when 2
+            return "Lactovegetariano"
+        when 3
+            return "Ovolactovegetariano"
+        when 4
+            return "Outros"
+        end
+    end 
 end
