f3ea1acfeea2eb85b3ad1cbe10f56ff986927709
Warley <wss@cin.ufpe.br>
2018-11-13 19:15:02 +0000

    Adicionado permissao no model e criado migracao para o model

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,8 @@ class Usuario < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/logo.png" #permissão de upolad para o paperclip
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
 end
