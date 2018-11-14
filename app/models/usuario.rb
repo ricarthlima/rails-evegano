2819c4ff2c633dc74c0c3a7278dd6333ec186d98
Warley <wss@cin.ufpe.br>
2018-11-14 17:56:18 +0000

    Erro de usuario corrigido

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,7 +4,4 @@ class Usuario < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/logo.png" #permissão de upolad para o paperclip
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  
 end
