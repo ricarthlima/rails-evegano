7d3a82a2207c2e0cee49821bbb170703e184a1a6
Ricarth Lima <rrsl@cin.ufpe.br>
2018-11-24 23:14:58 +0000

    caso bom, multiplo e ruim em harmonia

--- app/controllers/busca_controller.rb
+++ app/controllers/busca_controller.rb
@@ -7,6 +7,7 @@ class BuscaController < ApplicationController
 
     def buscar
         @nome_busca = params[:texto]
+        @proximos = leven(@nome_busca)
         
         if busca_vazia
             redirect_to inicio_path
@@ -25,9 +26,7 @@ class BuscaController < ApplicationController
             
         elsif @candidatos_componentes.size == 1 and (@candidatos_marcas.size == 0) and (@candidatos_produtos.size == 0)
             redirect_to(componentes_url+"/"+@candidatos_componentes[0].id.to_s)
-            
-        elsif @candidatos_componentes.size == 0 and @candidatos_produtos.size == 0 and @candidatos_marcas.size == 0
-            redirect_to(cadastro_pmc_path(@nome_busca))
+
         else
             render :index
         end
@@ -41,5 +40,32 @@ class BuscaController < ApplicationController
         else
             return false
         end
+    end
+    
+    def leven(busca)
+        proximos = Array.new
+        
+        Produto.all.each do |produto|
+            distancia = Levenshtein.distance(produto.nome,busca)
+            if distancia > 0 and distancia < 3
+                proximos << produto
+            end
+        end
+        
+        Marca.all.each do |marca|
+            distancia = Levenshtein.distance(marca.nome, busca)
+            if distancia > 0 and distancia < 3
+                proximos << marca
+            end
+        end
+        
+        Componente.all.each do |comp|
+            distancia = Levenshtein.distance(comp.nome, busca)
+            if distancia > 0 and distancia < 3
+                proximos << comp
+            end
+        end
+        
+        return proximos
     end
 end
