0050bcef65bf245b6d336884cf2bdfdabd5670a8
Ricarth Lima <rrsl@cin.ufpe.br>
2018-11-25 21:38:31 +0000

    força https na produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Forçar o HTTPS
+  config.force_ssl = true
 end
