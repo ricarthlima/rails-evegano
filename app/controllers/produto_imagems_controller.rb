e25f01c183919286b53b75865a23340163dd8e75
Warley <wss@cin.ufpe.br>
2018-11-26 21:01:52 +0000

    erro imagme

--- app/controllers/produto_imagems_controller.rb
+++ app/controllers/produto_imagems_controller.rb
@@ -11,6 +11,7 @@ class ProdutoImagemsController < ApplicationController
     img = RTesseract.new(@produto_imagem.cover.path, :lang => "por")
     #Imprime o texto convertido
     @ocr =  img.to_s
+    puts("----------------\n"+@ocr+"\n---------------")
     redirect_to controller: 'busca', action: 'buscar_ocr', ocr: @ocr
     @produto_imagem.cover = nil #Código para apagar a imagem, falta só resolver o redirect_to
     @produto_imagem.save
