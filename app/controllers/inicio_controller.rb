ddc2eaf4a9829b298fa5a9294e892d63d4da4de6
Ricarth Lima <rrsl@cin.ufpe.br>
2019-05-01 21:48:08 +0000

    cookie add

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,5 +1,14 @@
 class InicioController < ApplicationController
     def index
+        if !usuario_signed_in? then
+            if cookies[:first_time] == "true" then
+                render :index
+            else
+                cookies[:first_time] = "true"
+                render :welcome
+            end
+        end
+        
     end
     
     #before_action :authenticate_usuario!
