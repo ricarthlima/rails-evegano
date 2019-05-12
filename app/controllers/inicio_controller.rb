d5f8588eaad7d9d8a4912f47c642a32b4179b09f
Ricarth Lima <rrsl@cin.ufpe.br>
2019-05-12 02:13:12 +0000

    adicionada codificacao ao cookie

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,18 +1,9 @@
 class InicioController < ApplicationController
     def index
-        if !usuario_signed_in? then
-            if cookies[:first_time] == "true" then
-                render :index
-            else
-                render :welcome
-            end
-        end
-        
-    end
-    
-    def go
-        index
-        cookies[:first_time] = "true"
+        cookies.signed[:first_time] = {
+            value: "true",
+            expires: 5.day
+        }
     end
     
     #before_action :authenticate_usuario!
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,10 @@
 class WelcomeController < ApplicationController
-    cookies[:first_time] = "true"
+    def index
+        if usuario_signed_in? or cookies.signed[:first_time] == "true" then
+            redirect_to inicio_path
+        else
+            render :index
+        end
+    end
 end
+
